fd1b71a29b8f73082a5216f0fc4feb95d8076694
Gracia317 <graci4rm@gmail.com>
2026-05-03 19:42:27 +0300

    Add files via upload
    Un programme C: qui les 50 premiers nombres premiers dans un fichier, récupère les nombres dont le numéro de ligne et divisible par 3, change toutes les occurences de 5 en V et affiche les statsistiques.

---
+++ fichier2.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void premier();
+void stats(FILE *monfichier, int *U, int nbchange);
+
+void stats(FILE *monfichier, int *U, int nbchange)
+{
+  int l;
+  fprintf(monfichier,"============================================================\n");
+  fprintf(monfichier,"======Statistiques des changements des occurences de 5======\n");
+  fprintf(monfichier,"============================================================\n");
+  fprintf(monfichier,"Nombre de changements: %d\n", nbchange);
+  fprintf(monfichier,"Sur les lignes:\n");
+  for (l=0; l<nbchange; l++)
+  {
+      fprintf(monfichier,"%d\n", U[l]);
+  }
+}
+
+void premier()
+{
+  int i, j=0, compte_diviseur, ligne=0, n, temp, nbchange=0;
+  int l;
+  char T[17][4];
+  int U[10];//tableau hametrahana ny numero anle ligne misy changement es occurences de 5
+  FILE *monfichier;
+  monfichier = fopen("devoir1erMai.txt","w");
+  for (n=2; ligne<=50; n++)
+  {
+    compte_diviseur=0;
+    for(i=1; i<=n; i++)
+    {
+      if (n%i==0)
+      {
+        compte_diviseur++;
+      }
+    }
+    if (compte_diviseur==2)
+    {
+      ligne++;
+      if (ligne%3 == 0)
+      {
+        int nbchiffres = 0;
+        int k;
+        temp = n;//temporairement à calculer
+        while (temp > 0)
+        {
+          nbchiffres++;
+          temp = temp / 10;
+        }
+        temp = n;//andeha averina @ laoniny
+        k = nbchiffres - 1;
+        l=0;
+        while (temp > 0)
+        {
+          T[j][k] = (temp % 10) + '0';/*raha tsy misy +'0' dia manjary tsy ilay valeur no eo
+                                       fa zavatra hafa, merci my friend de me l'avoir rappelé*/
+          if (T[j][k] == '5')
+          {
+            T[j][k] = 'V';
+            U[nbchange]=ligne;
+            nbchange++;
+          }
+          k--;
+          temp = temp / 10;
+        }
+        fprintf(monfichier, "%s\n", T[j]);
+        j++;
+        T[j][nbchiffres] = '\0';
+      }
+    }
+  }
+  stats (monfichier, U, nbchange);
+  fclose (monfichier);
+}
+
+int main()
+{
+  premier();
+  return(0);
+}
